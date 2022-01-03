55f12032a4897501433f9931255708e8d3933e71
Chris Elias <chris.elias5@gmail.com>
2022-01-03 18:07:04 -0500

    Refactored config files

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 import {
   IntegrationInstanceConfigFieldMap,
   IntegrationInstanceConfig,
+  IntegrationExecutionContext,
+  IntegrationValidationError,
 } from '@jupiterone/integration-sdk-core';
+
+import { AddigyClient } from './addigy/client';
 
 export const instanceConfigFields: IntegrationInstanceConfigFieldMap = {
   clientId: {
@@ -29,7 +33,41 @@ export interface IntegrationConfig extends IntegrationInstanceConfig {
   clientSecret: string;
   /**
    * User data that has owner role
+   * Used to hit internal API endpoing
    */
   adminUsername: string;
   adminPassword: string;
+}
+
+export async function validateInvocation(
+  context: IntegrationExecutionContext<IntegrationConfig>,
+) {
+  const { instance, logger } = context;
+  const { config } = instance;
+
+  if (
+    !config.adminPassword ||
+    !config.adminUsername ||
+    !config.clientId ||
+    !config.clientSecret
+  ) {
+    throw new IntegrationValidationError(
+      'Config requires all of {adminPassword, adminUsername, clientId, clientSecret}',
+    );
+  }
+
+  const client = new AddigyClient({
+    clientId: config.clientId,
+    clientSecret: config.clientSecret,
+    adminUsername: config.adminUsername,
+    adminPassword: config.adminPassword,
+  });
+
+  try {
+    await client.fetchPolicies();
+  } catch (err) {
+    throw new IntegrationValidationError(
+      `Failed to authenticate with the addigy API: ${err.message}`,
+    );
+  }
 }
